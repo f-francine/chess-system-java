1d8669b5e00e5f14eca7d0e0a31479737b8066e4
f-francine <francinesguimaraes@outlook.com>
2020-06-16 04:08:12 -0300

    Pacing pieces on the board + UI

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,29 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
+
+public class ChessMatch {
+
+	private Board board;
+	
+	public ChessMatch() {
+		board = new Board(8, 8);
+		initialSetup();
+	}
+	
+	public ChessPiece[][] getPieces(){
+		ChessPiece[][] matrix = new ChessPiece[board.getRows()][board.getColumns()];
+		for(int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				matrix[i][j] = (ChessPiece)board.piece(i, j);
+			}
+		}
+		return matrix;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+	}
+}
